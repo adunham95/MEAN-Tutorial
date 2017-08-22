42d0a708bfd8a6d7fc851e2de351a0438c8662e0
adunham95 <Apd@21295>
2017-08-22 11:59:45 -0400

    Uploading Finished page

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const app = express();
 const users = require('./routes/users');
 
 // Port Number
-const port = 3000;
+const port = process.env.PORT || 8080;
 
 // CORS Middleware
 app.use(cors());
@@ -45,6 +45,10 @@ app.use('/users', users);
 // Index Route
 app.get('/', (req, res) =>{
     res.send("Invaled End point")
+});
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'public/index.html'))
 });
 
 // Start Server
